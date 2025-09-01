a6df446f3c696a43f3ee4af1604e094d3ec3b11b
Echevensko <empresas.echevensko@gmail.com>
2025-09-01 17:33:19 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import type { ReactNode } from "react";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Echevensko Empresas",
-  description: "Charla corporativa – La Magia de la Imaginación",
+  title: "Charla corporativa – La Magia de la Imaginación",
+  description: "Echevensko Empresas",
   icons: {
     icon: "/images/favicon.png",
     shortcut: "/images/favicon.png",
