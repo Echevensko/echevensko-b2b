2bd33f84efdf779c6f91f1e57fcb81fccda77635
Echevensko <empresas.echevensko@gmail.com>
2025-09-01 17:27:54 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Echevensko Empresas",
-  description: "Charlas corporativas – La Magia de la Imaginación",
+  description: "Charla corporativa – La Magia de la Imaginación",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
